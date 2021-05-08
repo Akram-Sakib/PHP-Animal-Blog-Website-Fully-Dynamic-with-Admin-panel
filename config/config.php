ce9c50e36288fe873424afc3e28d0f648bed220b
Akram-Sakib <74033438+Akram-Sakib@users.noreply.github.com>
2021-05-08 21:37:18 +0600

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "db_animal");
+define("TITLE", "Animal Blog");
+define("KEYWORDS", "Animal Biography, Domestic Animal, Loving Animal");
+
+/* @8tG@UDH,Ca1 */
